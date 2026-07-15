a385e201e93556be70a18c5fbfaf6e36d8e9a7c7
Error-Cezar <mobilesgame55@proton.me>
2026-07-15 13:15:03 +0200

    chore: removed parser

--- parser.py
+++
@@ -1,8 +0,0 @@
-from argparse import ArgumentParser
-
-
-def parse(parser: ArgumentParser):
-    parser.add_argument("--dev", help="Run in development mode", action="store_true")
-    parser.add_argument("--model", help="Model to use", default="gemma4:e4b")
-    parser.add_argument("--port", required=False, help="Port to run the server on", default=5764)
-    parser.add_argument("--host", required=False, help="Host to run the server on", default="0.0.0.0")
